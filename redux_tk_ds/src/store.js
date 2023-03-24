fb6bb69cb62d1ce4ec0feb7775abcd8438a33f4b
chitranssh20 <saxenaayush2052001@gmail.com>
2023-03-24 19:31:12 +0530

    Cross Tab Synchronization Achieved

--- redux_tk_ds/src/store.js
+++ redux_tk_ds/src/store.js
@@ -5,12 +5,15 @@ import dataSlice from "./dataSlice";
 
 const config = {
   // TOGGLE_TODO will not be triggered in other tabs
-  blacklist: ['updateValue'],
+  blacklist: ['data/updateValue'],
 };
+const check = {}
 const middlewares = createStateSyncMiddleware(config);
-// console.log(middlewares);
+console.log('middlewares', middlewares);
+// console.log('default middlewares', typeof(defaults));
+// console.log('default middlewares', (defaults));
 const store = configureStore({
-   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(middlewares),
+  middleware: (getDefaultMiddleware) => [...getDefaultMiddleware(), middlewares],
   reducer: {
     data: dataSlice
   },
